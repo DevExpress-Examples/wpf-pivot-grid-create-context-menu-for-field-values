7f8ebfa337499ddb0f96ec3f1a1a8bb230d54431
Boris Zaitsev <bvzai@users.noreply.github.com>
2018-05-24 20:23:03 +0300

    Add brand new content for 18.1.3

---
+++ CS/WpfPivotGrid_CustomMenu/DataHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+
+namespace WpfPivotGrid_CustomMenu
+{
+    static class DataHelper
+    {
+        public static  DataTable CreatePivotDataSource()
+        {
+            DataTable myTable = new DataTable();
+            myTable.Columns.Add("Name", typeof(string));
+            myTable.Columns.Add("Type", typeof(string));
+            myTable.Columns.Add("Value", typeof(decimal));
+            myTable.Columns.Add("Target", typeof(decimal));
+
+            myTable.Rows.Add(new object[] { "Aaa", "Type 1", 7, 7 });
+            myTable.Rows.Add(new object[] { "Aaa", "Type 1", 4, 12 });
+            myTable.Rows.Add(new object[] { "Bbb", "Type 1", 12, 13 });
+            myTable.Rows.Add(new object[] { "Bbb", "Type 1", 14, 12 });
+            myTable.Rows.Add(new object[] { "Ccc", "Type 1", 11, 12 });
+            myTable.Rows.Add(new object[] { "Ccc", "Type 1", 10, 12 });
+
+            myTable.Rows.Add(new object[] { "Aaa", "Type 2", 4, 6 });
+            myTable.Rows.Add(new object[] { "Aaa", "Type 2", 2, 5 });
+            myTable.Rows.Add(new object[] { "Bbb", "Type 2", 3, 4 });
+            myTable.Rows.Add(new object[] { "Bbb", "Type 2", 1, 4 });
+            myTable.Rows.Add(new object[] { "Ccc", "Type 2", 8, 5 });
+            myTable.Rows.Add(new object[] { "Ccc", "Type 2", 22, 13 });
+
+            return myTable;
+        }
+    }
+}
---
+++ CS/WpfPivotGrid_CustomMenu/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Xpf.PivotGrid;
+using System.Windows;
+
+namespace WpfPivotGrid_CustomMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            pivotGrid.DataSource = DataHelper.CreatePivotDataSource();
+        }
+
+        private void CopyFieldElementData_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            PivotGridFieldValueMenuInfo menuInfo = pivotGrid.GridMenu.MenuInfo as PivotGridFieldValueMenuInfo;
+            if (menuInfo != null && menuInfo.FieldValueElementData != null &&
+                menuInfo.FieldValueElementData.Value.ToString() != string.Empty)
+            {
+                Clipboard.SetDataObject(menuInfo.FieldValueElementData.Value);
+            }
+        }
+
+        private void FilterFieldElementData_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            PivotGridFieldValueMenuInfo menuInfo = pivotGrid.GridMenu.MenuInfo as PivotGridFieldValueMenuInfo;
+            if (menuInfo != null && menuInfo.FieldValueElementData != null &&
+                menuInfo.FieldValueElementData.Value != null &&
+                menuInfo.FieldValueElementData.Field != null)
+            {
+                PivotGridField field = menuInfo.FieldValueElementData.Field;
+                object value = menuInfo.FieldValueElementData.Value;
+                field.FilterValues.FilterType = FieldFilterType.Included;
+                field.FilterValues.Add(value);
+            }
+        }
+
+        private void CopyCellElementData_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            PivotGridCellMenuInfo menuInfo = pivotGrid.GridMenu.MenuInfo as PivotGridCellMenuInfo;
+            if (menuInfo != null && menuInfo.CellElementData != null &&
+                menuInfo.CellElementData.Value.ToString() != string.Empty)
+            {
+                Clipboard.SetDataObject(menuInfo.CellElementData.DisplayText);
+            }
+        }
+    }
+}
