5d88dfeab924fa3eb3e38addc93526369295eab9
Boris Zaitsev <bvzai@users.noreply.github.com>
2018-05-25 12:02:41 +0300

    Added HeaderMenuCustomizations and HeaderAreaMenuCustomizations

--- CS/WpfPivotGrid_CustomMenu/DataHelper.cs
+++ CS/WpfPivotGrid_CustomMenu/DataHelper.cs
@@ -8,23 +8,24 @@ namespace WpfPivotGrid_CustomMenu
         {
             DataTable myTable = new DataTable();
             myTable.Columns.Add("Name", typeof(string));
+            myTable.Columns.Add("Owner", typeof(string));
             myTable.Columns.Add("Type", typeof(string));
             myTable.Columns.Add("Value", typeof(decimal));
             myTable.Columns.Add("Target", typeof(decimal));
 
-            myTable.Rows.Add(new object[] { "Aaa", "Type 1", 7, 7 });
-            myTable.Rows.Add(new object[] { "Aaa", "Type 1", 4, 12 });
-            myTable.Rows.Add(new object[] { "Bbb", "Type 1", 12, 13 });
-            myTable.Rows.Add(new object[] { "Bbb", "Type 1", 14, 12 });
-            myTable.Rows.Add(new object[] { "Ccc", "Type 1", 11, 12 });
-            myTable.Rows.Add(new object[] { "Ccc", "Type 1", 10, 12 });
+            myTable.Rows.Add(new object[] { "Aaa", "Andrew Fuller", "Type 1", 7, 7 });
+            myTable.Rows.Add(new object[] { "Aaa", "Anne Dodsworth","Type 1", 4, 12 });
+            myTable.Rows.Add(new object[] { "Bbb", "Janet Leverling","Type 1", 12, 13 });
+            myTable.Rows.Add(new object[] { "Bbb", "Andrew Fuller", "Type 1", 14, 12 });
+            myTable.Rows.Add(new object[] { "Ccc", "Anne Dodsworth", "Type 1", 11, 12 });
+            myTable.Rows.Add(new object[] { "Ccc", "Janet Leverling", "Type 1", 10, 12 });
 
-            myTable.Rows.Add(new object[] { "Aaa", "Type 2", 4, 6 });
-            myTable.Rows.Add(new object[] { "Aaa", "Type 2", 2, 5 });
-            myTable.Rows.Add(new object[] { "Bbb", "Type 2", 3, 4 });
-            myTable.Rows.Add(new object[] { "Bbb", "Type 2", 1, 4 });
-            myTable.Rows.Add(new object[] { "Ccc", "Type 2", 8, 5 });
-            myTable.Rows.Add(new object[] { "Ccc", "Type 2", 22, 13 });
+            myTable.Rows.Add(new object[] { "Aaa", "Andrew Fuller", "Type 2", 4, 6 });
+            myTable.Rows.Add(new object[] { "Aaa", "Anne Dodsworth", "Type 2", 2, 5 });
+            myTable.Rows.Add(new object[] { "Bbb", "Janet Leverling", "Type 2", 3, 4 });
+            myTable.Rows.Add(new object[] { "Bbb", "Andrew Fuller", "Type 2", 1, 4 });
+            myTable.Rows.Add(new object[] { "Ccc", "Anne Dodsworth", "Type 2", 8, 5 });
+            myTable.Rows.Add(new object[] { "Ccc", "Janet Leverling", "Type 2", 22, 13 });
 
             return myTable;
         }
