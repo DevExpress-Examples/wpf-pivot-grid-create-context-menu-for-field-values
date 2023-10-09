ff48c090dbed242fb0caa2525c90b39d5ff744f2
DevExpressExampleBot <fakeEmail>
2023-10-09 16:14:51 +0400

    Source auto update [skip ci]

--- VB/CreateValueContextMenu/MainWindow.xaml.vb
+++ VB/CreateValueContextMenu/MainWindow.xaml.vb
@@ -1,47 +1,37 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
-Imports DevExpress.Xpf.PivotGrid.Internal
 
 Namespace HowToBindToMDB
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
-        Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
+
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
 
         Public Sub New()
-            InitializeComponent()
-            pivotGridControl1.DataSource = salesPersonDataTable
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
         End Sub
 
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
             salesPersonDataAdapter.Fill(salesPersonDataTable)
         End Sub
 
-        Private Sub CopyValueItem_ItemClick(ByVal sender As Object, _
-											ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            Dim menuInfo As PivotGridFieldValueMenuInfo = _
-				TryCast(pivotGridControl1.GridMenu.MenuInfo, PivotGridFieldValueMenuInfo)
-            If menuInfo IsNot Nothing AndAlso menuInfo.ValueItem IsNot Nothing AndAlso _
-				menuInfo.ValueItem.Value.ToString() <> String.Empty Then
-					Clipboard.SetText(menuInfo.ValueItem.Value.ToString())
-            End If
+        Private Sub CopyValueItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Dim menuInfo As PivotGridFieldValueMenuInfo = TryCast(Me.pivotGridControl1.GridMenu.MenuInfo, PivotGridFieldValueMenuInfo)
+            If menuInfo IsNot Nothing AndAlso menuInfo.ValueItem IsNot Nothing AndAlso Not Equals(menuInfo.ValueItem.Value.ToString(), String.Empty) Then Call Clipboard.SetText(menuInfo.ValueItem.Value.ToString())
         End Sub
 
-        Private Sub FilterValueItem_ItemClick(ByVal sender As Object, _
-											  ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            Dim menuInfo As PivotGridFieldValueMenuInfo = _
-				TryCast(pivotGridControl1.GridMenu.MenuInfo, PivotGridFieldValueMenuInfo)
-            If menuInfo IsNot Nothing AndAlso menuInfo.ValueItem IsNot Nothing AndAlso _
-				menuInfo.ValueItem.Value IsNot Nothing AndAlso _
-				menuInfo.ValueItem.Field IsNot Nothing Then
-					Dim field As PivotGridField = menuInfo.ValueItem.Field
-					Dim value As Object = menuInfo.ValueItem.Value
-					field.FilterValues.FilterType = FieldFilterType.Included
-					field.FilterValues.Add(value)
+        Private Sub FilterValueItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Dim menuInfo As PivotGridFieldValueMenuInfo = TryCast(Me.pivotGridControl1.GridMenu.MenuInfo, PivotGridFieldValueMenuInfo)
+            If menuInfo IsNot Nothing AndAlso menuInfo.ValueItem IsNot Nothing AndAlso menuInfo.ValueItem.Value IsNot Nothing AndAlso menuInfo.ValueItem.Field IsNot Nothing Then
+                Dim field As PivotGridField = menuInfo.ValueItem.Field
+                Dim value As Object = menuInfo.ValueItem.Value
+                field.FilterValues.FilterType = FieldFilterType.Included
+                field.FilterValues.Add(value)
             End If
         End Sub
     End Class
